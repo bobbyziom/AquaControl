4c74b907a90527bfb5cf030763892016988c4320
bobbytech <unknown-user@172.30.213.125>
2014-02-27 11:37:41 +0100

    Initial check-in of module OOP2

---
+++ OOP2/GayKevin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP2
+{
+	public class GayKevin {
+
+		/// <summary>
+		/// The size of the penis.
+		/// </summary>
+
+		public int penisSize;
+		public int width;
+
+		public GayKevin () {
+
+			// constructor
+			Console.WriteLine ("THIS IS CONSTRUCTOR");
+
+		}
+
+		public GayKevin(int penis, int w) {
+
+			this.penisSize = penis;
+			this.width = w;
+
+		}
+
+	}
+
+}
+
---
+++ OOP2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP2
+{
+
+
+	public class Program
+	{
+
+		public static void Main(string[] args)
+		{
+
+			GayKevin kevin = new GayKevin (3, 5);
+
+			Console.Write ("HEJ KEVIN! Din penis er " + kevin.penisSize + "mm lang, og " + kevin.width + "mm bred.. noob!");
+
+
+
+		}
+	}
+}
+
