024d7f5097f5f4cdbbaf243e7741eb9405e6a28f
bobbyziom <Bobby@Bobbys-MacBook-Pro-893.local>
2014-03-06 16:12:31 +0100

    mqtttest

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -4,23 +4,31 @@ using Nmqtt;
 namespace OOP2
 {
 
-
 	public class Program
 	{
-
-		public MqttClient client = new Nmqtt.MqttClient("test.mosquitto.org", 1883, "Nmqtt_quickstart");
-
+	
 		public static void Main(string[] args)
 		{
+			// mqtt test:
+			Console.WriteLine ("connecting....");
+
+			// connecting to server:
+			var client = new Nmqtt.MqttClient("test.mosquitto.org", 1883, "Nmqtt_quickstart");
+			ConnectionState connectionState = client.Connect();
+
+			// set up subscription
+			IObservable<MqttRecivedMessage<byte[]> observation = client.ListenTo("Nmqtt_quickstart_topic", MqttQos.AtMostOnce);
 
 
+
+			// making data to send
 			byte[] msgData = new byte[] { 1, 2, 3 };
 
-			this.client.PublishMessage ("bobby/chop/1/status/lol", msgData);
+			// send data to topic
+			client.PublishMessage ("bobby/chop/1/status/lol", msgData);
 
-			GayKevin kevin = new GayKevin (2, 1);
-
-			Console.Write ("HEJ KEVIN! Din penis er " + kevin.penisSize + "mm lang, og " + kevin.width + "mm bred.. noob!");
+			// print data sent to console
+			Console.WriteLine ("  data sent!");
 
 
 
