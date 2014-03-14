4058c9bfe3ec51bf1c6ff2a08be23c3849853193
bobbyziom <Bobby@h279.aau1x-2.klient.sydhavn.site.aau.dk>
2014-03-14 11:20:35 +0100

    xively connected
    HIDDEN API KEY

---
+++ xively_test/MainWindow.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Gtk;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using xively_test;
+
+public partial class MainWindow: Gtk.Window
+{
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButtonGetDataClicked (object sender, EventArgs e)
+	{
+		// create web request to xively
+		WebRequest request = WebRequest.Create("https://api.xively.com/v2/feeds/1742736382.json");
+		// add api key, for verification
+
+		Console.WriteLine (APIentry.Text);
+
+		request.Headers.Add ("X-ApiKey", APIentry.Text);
+
+		// get response from xively
+		WebResponse response = request.GetResponse ();
+		// write data into stream
+		Stream dataStream = response.GetResponseStream ();
+		// create reader
+		StreamReader reader = new StreamReader (dataStream);
+		// copy recieved data into str
+		string responseFromServer = reader.ReadToEnd ();
+		// write recieved into console
+		Console.WriteLine (responseFromServer);
+		// collect and store data 
+		XivelyData info = JsonConvert.DeserializeObject<XivelyData> (responseFromServer);
+		// write data streams and values to console
+
+
+		label1.Text = info.datastreams [0].id + ": ";
+		label2.Text = info.datastreams [0].current_value;
+
+		label7.Text = info.datastreams [1].id + ": ";
+		label8.Text = info.datastreams [1].current_value;
+
+		label9.Text = info.datastreams [2].id + ": ";
+		label10.Text = info.datastreams [2].current_value;
+
+		label11.Text = info.datastreams [3].id + ": ";
+		label12.Text = info.datastreams [3].current_value;
+
+		label13.Text = info.datastreams [4].id + ": ";
+		label14.Text = info.datastreams [4].current_value;
+
+		label15.Text = info.datastreams [5].id + ": ";
+		label16.Text = info.datastreams [5].current_value;
+
+		label17.Text = info.datastreams [6].id + ": ";
+		label18.Text = info.datastreams [6].current_value;
+
+		label19.Text = info.datastreams [7].id + ": ";
+		label20.Text = info.datastreams [7].current_value;
+
+		label21.Text = info.datastreams [8].id + ": ";
+		label22.Text = info.datastreams [8].current_value;
+
+		label23.Text = info.datastreams [9].id + ": ";
+		label24.Text = info.datastreams [9].current_value;
+
+		label25.Text = info.datastreams [10].id + ": ";
+		label26.Text = info.datastreams [10].current_value;
+
+		label27.Text = info.datastreams [11].id + ": ";
+		label28.Text = info.datastreams [11].current_value;
+
+		label29.Text = info.datastreams [12].id + ": ";
+		label30.Text = info.datastreams [12].current_value;
+			
+	}
+}
---
+++ xively_test/XivelyData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace xively_test
+{
+	public class Unit
+	{
+		public string symbol { get; set; }
+		public string label { get; set; }
+	}
+
+	public class Datastream
+	{
+		public string id { get; set; }
+		public string current_value { get; set; }
+		public string at { get; set; }
+		public string max_value { get; set; }
+		public string min_value { get; set; }
+		public Unit unit { get; set; }
+	}
+
+	public class XivelyData
+	{
+		public int id { get; set; }
+		public string title { get; set; }
+		public string @private { get; set; }
+		public string feed { get; set; }
+		public string status { get; set; }
+		public string updated { get; set; }
+		public string created { get; set; }
+		public string creator { get; set; }
+		public string version { get; set; }
+		public List<Datastream> datastreams { get; set; }
+		public string product_id { get; set; }
+		public string device_serial { get; set; }
+	}
+}
+
