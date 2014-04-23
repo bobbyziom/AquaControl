b55df202ed99a891f5806c0d96bf6dfb8fe01dca
bobbyziom <Bobby@Macintosh.local>
2014-04-23 20:05:43 +0200

    connection class (internet connectivity tester)
    staying alive class to ensure internet connectivity

---
+++ AquaControl/Connection.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Timers;
+
+namespace oose_testster
+{
+	public class Connection
+	{
+
+		private Timer _update;
+		private bool _isAlive;
+
+		/// <summary>
+		/// Gets or sets a value indicating whether program is connected.
+		/// </summary>
+		/// <value><c>true</c> if this instance is connected; otherwise, <c>false</c>.</value>
+		public bool IsConnected { get; set; }
+
+		/// <summary>
+		/// Gets or sets the test ping internet address.
+		/// </summary>
+		/// <value>The test adrdess.</value>
+		public string TestAdress { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="oose_testster.Connection"/> class. </p>
+		/// Setting default address to: www.google.com </br>
+		/// Checking connection every 60000 ms.
+		/// </summary>
+		public Connection() 
+		{
+
+			TestAdress = "www.google.com";
+
+			_update = new Timer (10000);
+			_update.Start ();
+			_update.AutoReset = true;
+			_update.Elapsed += new ElapsedEventHandler(OnTimerTick);
+
+		}
+
+		/// <summary>
+		/// Checks for internet connection.
+		/// </summary>
+		/// <returns><c>true</c>, if connection to internet, <c>false</c> otherwise.</returns>
+		public bool CheckConnection() 
+		{
+
+			return _isAlive;
+
+		}
+
+		/// <summary>
+		/// Pings the host.
+		/// </summary>
+		/// <returns><c>true</c>, if host was pinged, <c>false</c> otherwise.</returns>
+		/// <param name="address">Address.</param>
+		private static bool PingHost(string address)
+		{
+
+			bool pingable = false;
+			Ping pinger = new Ping();
+
+			try
+			{
+				PingReply reply = pinger.Send(address);
+
+				pingable = reply.Status == IPStatus.Success;
+			}
+			catch (PingException)
+			{
+				return false;
+			}
+
+			return pingable;
+
+		}
+
+		/// <summary>
+		/// Raises the timer tick event.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
+		private void OnTimerTick(object sender, EventArgs e)
+		{
+
+			_isAlive = PingHost (TestAdress);
+			Console.WriteLine (_isAlive);
+
+		}
+			
+	}
+}
+
