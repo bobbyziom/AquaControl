a00168406fbff336a1815bd8496f7487cc37afce
Maretty21 <maretty21@gmail.com>
2014-04-24 15:43:11 +0200

    Awesome PH widget + insane color

--- AquaControl/Widgets/PHWidget.cs
+++ AquaControl/Widgets/PHWidget.cs
@@ -5,6 +5,13 @@ namespace AquaControl
 	public class PHWidget : DatapointObject
 	{
 
+		float fadecolor = 0;
+		string phTextShow = "PH";
+		int textMoveX;
+		int textMoveY;
+		float PHValue = 3.0f;
+		float PHcolorR = 0;
+		float PHcolorB = 0;
 
 		public PHWidget () 
 		{
@@ -22,27 +29,73 @@ namespace AquaControl
 			X = x;
 			Y = y;
 		
+			surface.SetSourceRGBA (1, 1, 1, 0.1);
+			surface.Arc (X, Y, Radius, 0, Math.PI * 2);
+			surface.Fill ();
+
 			surface.CurveTo(X-100, Y-10, X-60, Y+10, X-30, Y-10);
 			surface.CurveTo(X-30, Y-10, X, Y+10, X+30, Y-10);
 			surface.CurveTo(X+30, Y-10, X+60, Y+10, X+100, Y-10);
-			surface.SetSourceRGB (0.1, 0.1, 0.2);
+			surface.SetSourceRGB (0.1, 0.1, 0.1);
 			surface.Arc (X, Y, Radius, 0, Math.PI * 1);
-			surface.Fill ();
-
-			surface.SetSourceRGBA (1, 1, 1, 0.3);
-			surface.Arc (X, Y, Radius, 0, Math.PI * 1);
-			surface.CurveTo (X - 100, Y, X, Y+100, X + 100, Y);
 			surface.Fill ();
 
 			//XivelyData data = XivelyData.GetCurrentData (apiKey, feedId);
 
 			surface.SetSourceRGBA (1, 1, 1, 0.1);
-			surface.MoveTo (X-32, Y+50);
-			surface.SetFontSize (50);
-			surface.ShowText ("PH");
+			surface.MoveTo (X+textMoveX, Y+textMoveY);
+			surface.SetFontSize (80);
+			surface.ShowText (phTextShow);
 
 			//surface.Arc (X, Y, Radius, 0, Math.PI * 1);
 			surface.Fill();
+
+			surface.CurveTo(X-100, Y-10, X-60, Y+10, X-30, Y-10);
+			surface.CurveTo(X-30, Y-10, X, Y+10, X+30, Y-10);
+			surface.CurveTo(X+30, Y-10, X+60, Y+10, X+100, Y-10);
+			surface.SetSourceRGBA (PHcolorR, 0, PHcolorB, fadecolor);
+			surface.Arc (X, Y, Radius, 0, Math.PI * 1);
+			surface.Fill ();
+
+
+		}
+
+		public override void OnHoverAction ()
+		{
+			// MOVE TO ALIGN NUMBER
+			if (PHValue < 10) {
+				textMoveX = -22;
+				textMoveY = 30;
+			} else{
+				textMoveX = -48;
+				textMoveY = 30;
+			}
+
+			// SPECIFIC COLOR	
+			PHcolorR = 1-(PHValue/7);
+			PHcolorB = PHValue/7-1;	
+			Console.WriteLine (PHcolorR+" "+PHcolorB);
+
+
+			// FADE IN
+			if (fadecolor < 0.3) {	
+				fadecolor += 0.01f; 
+				phTextShow =  Convert.ToString(PHValue);
+			}
+
+		}
+
+		public override void OnNoHoverAction()
+		{
+			if (fadecolor > 0) {
+				fadecolor -= 0.005f;
+			} else {
+			
+			}
+
+			phTextShow = "PH";
+			textMoveX = -55;
+			textMoveY = 30;
 
 
 		}
